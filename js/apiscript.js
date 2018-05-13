1c9bcec454f31ae81467d78c3c8c32a465ecbaf3
Idan Damti <22159603+damtii@users.noreply.github.com>
2018-05-13 20:56:18 +0300

    first commite

---
+++ js/apiscript.js
@@ -0,0 +1,325 @@
+$(document).ready(function () {
+  console.log("api script file is ready!");
+
+
+  $(".showhideplayer").hide(); // hiding the main player by default. 
+
+
+
+
+
+    function showAllPlaylist () {
+
+  $.get("http://localhost:8080/ajaxproject/api/playlist", function (data, status) {
+    let myresults = "";
+    let mainPlayerContent = '';
+    let name = '';
+    let image = '';
+    let id = 0;
+    let songs = 'songs';
+    for (let item in data['data']) {
+      name = data['data'][item]['name'];
+      image = data['data'][item]['image'];
+      songs = data['data'][item]['songs'];
+      id = data['data'][item]['id'];
+
+      myresults += `       
+                <div class="col-sm-3 mx-auto">
+                    <div class="container text-center">
+                        <div class="arc-wrapper">
+                            <h3 class="round">${name}</h3>                 
+                            <div class="overlay">  
+                                <div class="row">                                                                      
+                                    <div class="col-sm-6 mx-auto editicon">                                                                        
+                                        <a href="#edit" data-toggle="modal" data-id=${id} data-target="#editPlaylist">                   
+                                            <i class="far fa-edit" data-id=${id}></i>                                           
+                                        </a>                                        
+                                    </div> 
+                                    <div class="col-sm-6 mx-auto deleteicon"> 
+                                        <a href="#delete" data-toggle="modal" data-id=${id} data-target="#deletePopup">                                           
+                                            <i class="fas fa-trash" data-id=${id}></i>                        
+                                        </a>
+                                    </div>  
+                                </div>                  
+                                <div class="col-sm-12 mx-auto playicon">
+                                    <a href="#play">
+                                        <i class="fas fa-play-circle"></i>                        
+                                    </a>
+                                </div> 
+                                
+                                
+                            </div>
+                            <div class="image">
+                                <img src="${image}" class="rounded-circle" alt="${name}" >
+                            </div>                
+                        </div>
+                    </div> 
+                </div> 
+                
+                `;
+
+      mainPlayerContent = `
+  <!-- start of main player -->
+  
+    <div class="playerEditDeleteIcons"></div>
+    <div class="mainplayer">
+        <div class="row">
+            <div class="col-sm-4">  
+                    <div class="playiconbig">
+                        <a href="#play">
+                            <i class="fas fa-play-circle"></i>                        
+                        </a>
+                    </div>                 
+                    <div class="col-sm-12 mx-auto pauseiconbig">
+                        <a href="#pause">
+                            <i class="fas fa-pause-circle"></i>                       
+                        </a>
+                    </div> 
+  
+                    <div class="roundimage" id="rotate"></div>             
+            </div>
+            <div class="col-sm-8">
+                <div class="myplayer">
+                    <audio controls>
+                        <source src="http://rockdesign.co.il/songs/01.mp3" type="audio/mpeg">             
+                            Your browser does not support the audio element.
+                    </audio>
+                </div>
+  
+                <div class="belowplayer">                                
+                    <p id="songName">Now Playing: Love Yourself</p>
+                    <ol>
+                        <li>song 1</li>
+                        <li>song 2</li>
+                        <li>song 3</li>
+                        <li>song 4</li>
+                        <li>song 5</li>
+                        <li>song 6</li>
+                    </ol> 
+                </div>
+            </div>
+        </div>
+    </div>
+  <!-- end of main player -->
+  `;
+    }
+
+    $('.showhideplayer').html(mainPlayerContent); // loading the main player to the HTML 
+    $("#allplaylist").html(myresults); // Loading the playlists to the HTML
+
+    // make the title round
+    $(".round").arctext({radius: 120});
+
+
+    // change the play icon to pause icon
+    $(".playicon").click(function () {        
+      $(".playiconbig").hide();
+      $(".pauseiconbig").show();
+    });
+
+    // show on the player the right info of the selected playlist
+
+    $('.arc-wrapper').click(function () {
+
+        let id = $(this).find('.editicon a').attr('data-id');
+        let idToChange = `
+        <span class="myplayericons" data-toggle="modal" data-id="${id}" data-target="#editPlaylistSongs">                   
+            <i class="fas fa-pencil-alt" data-id='${id}'></i>                                             
+        </span>
+        <br>
+        <span class="myplayericons" data-toggle="modal" data-id="${id}" data-target="#deletePopup">                                           
+            <i class="fas fa-trash" data-id="${id}"></i>                        
+        </span>        
+        `;
+        $('.playerEditDeleteIcons').html(idToChange);
+
+        let image = $(this).find('.image img').attr('src');
+        let imageToChange = `<img src="${image}">`;
+        $('#rotate').html(imageToChange);
+        if ($('#rotate2')) {
+            $('#rotate2').html(imageToChange);
+            $('#rotate2').attr('id', 'rotate');
+        }
+    });
+
+  });
+};
+showAllPlaylist();
+
+//   saving new playlist
+
+  $('#savePlayList').click(function () {
+
+    let playListName = $('#playlistName').val();
+    let playListUrl = $('#playlistUrl').val();
+    let songs = [];
+    $('#songListWrapper').children().each(function (index) {
+
+      let songName = '';
+      let songUrl = '';
+      $('input', $(this)).each(function () {
+        if ($(this).data("id") === 'songUrl') {
+          songUrl = $(this).val();
+        }
+        if ($(this).data("id") === 'songName') {
+          songName = $(this).val();
+        }
+      });
+      songs.push({"name": songName, "url": songUrl});
+    });
+
+
+    let data = {
+      "name": playListName,
+      "image": playListUrl,
+      "songs": songs
+    };
+
+
+    $.ajax({
+      type: "POST",
+      url: 'http://localhost:8080/ajaxproject/api/playlist',
+      data: data,      
+      success: function (data, status){
+        console.log(data);
+
+//adding the get call in order to see the new added playlist
+
+    $('input').val(""); //reset the values of the fields 
+
+        showAllPlaylist();
+
+      },     
+      failure: function(errMsg) {
+        alert("error on sending JSON");     
+    }
+    });    
+
+  });
+
+
+
+// Delete single playlist
+      
+
+      $( "div" ).on( "click", ".fa-trash", function() {          
+        let id = $(this).attr('data-id');
+        function deletePlaylist(){
+        $.ajax({
+            url: `http://localhost:8080/ajaxproject/api/playlist/${id}`,
+            type: 'DELETE',
+            success: function(result) {                  
+                showAllPlaylist();                            
+            }
+        });
+    }
+    $('.deletePlaylist').click(function () { 
+        deletePlaylist();
+    });
+});
+
+//End Of Delete single playlist
+
+// edit the playlist info
+
+$( "div" ).on( "click", ".fa-edit", function() {  
+    var id = $(this).attr('data-id');
+    var myresults = "";
+    var name = '';
+    var image = '';
+               
+    $.get(`http://localhost:8080/ajaxproject/api/playlist/${id}`, function (data, status) {   
+            
+        name = data['data']['name'];
+        image = data['data']['image'];         
+        id = data['data']['id'];   
+       
+        $('#playlistSingleName').val(name);
+        $('#playlistSingleUrl').val(image);
+        $('#playlistSingleImg').attr('src', image);
+
+        });   
+  });
+
+   // End of edit the playlist info
+
+
+// updating the playlist name and image and saving in DB
+
+$( "div" ).on( "click", ".fa-edit", function() {  
+    var id = $(this).attr('data-id');
+
+$('#updatePlaylist').click(function () {
+    let playlistSingleName = $('#playlistSingleName').val();
+    let playlistSingleUrl = $('#playlistSingleUrl').val();   
+    let data = {
+      "name": playlistSingleName,
+      "image": playlistSingleUrl      
+    };
+$.ajax({
+    type: "POST",
+    url: `http://localhost:8080/ajaxproject/api/playlist/${id}`,
+    data: data,      
+    success: function (data, status){      
+
+//adding the get call in order to see the new added playlist
+
+    $('input').val(""); //reset the values of the fields 
+    showAllPlaylist();
+    },     
+    failure: function(errMsg) {
+      alert("error on sending JSON");     
+  }
+  });    
+}); 
+}); 
+
+
+// END OF updating the playlist name and image
+
+// Update the song list of a single playlist
+
+$( "div" ).on( "click", ".fa-pencil-alt", function() {  
+    let id = $(this).attr('data-id');
+    let songName = '';
+    let songUrl = '';
+    let songs = [];
+               
+    $.get(`http://localhost:8080/ajaxproject/api/playlist/${id}/songs`, function (data, status) {           
+        for (let item in data['data']['songs']) {
+        songName = data['data']['songs'][item]['name'];
+        songUrl = data['data']['songs'][item]['url']; 
+        console.log(songName);
+        console.log(songUrl); 
+        
+        $( "input" ).each(function( ) {
+            if ($(this).data("id") === 'songUrl') {
+                $('.songUrl').val(songUrl);
+            }
+            if ($(this).data("id") === 'songName') {
+                $('.songName').val(songName);
+            }
+          });
+
+         
+        }
+    });        
+});
+    
+    
+// END OF Update the song list of a single playlist
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
---
+++ js/script.js
@@ -0,0 +1,92 @@
+$(document).ready(function () {
+  console.log("script file is ready!");
+
+  $(".round").arctext({radius: 120});
+
+  $(".addanother").click(function () {
+    let song = `
+          <div class="row list">
+            <div class="col">
+                <input type="text" class="form-control" data-id="songUrl" placeholder="Song URL">
+              </div>
+              <div class="col">
+                <input type="text" class="form-control" data-id="songName" placeholder="Song Name">
+              </div>
+          </div>`;
+    $('#songListWrapper').append(song);
+
+  });
+
+  $(".addanother").click(function () {
+    let song = `
+          <div class="row list">
+            <div class="col">
+                <input type="text" class="form-control" data-id="songUrl" placeholder="Song URL">
+              </div>
+              <div class="col">
+                <input type="text" class="form-control" data-id="songName" placeholder="Song Name">
+              </div>
+          </div>`;
+    $('#songSingleListWrapper').append(song);
+
+  });
+
+  $(".nav-link").click(function () {
+    $(".step2").hide();
+    $(".step1").show();
+  });
+
+  $(".nextstep").click(function () {
+    $(".step1").hide();
+    $(".step2").show();
+  });
+
+ 
+
+  $("#allplaylist").on("click", ".playicon", function () {
+    $(".showhideplayer").slideUp();
+    $(".showhideplayer").slideDown();
+  });
+
+  $(".playicon").click(function () {
+    $(".playiconbig").hide();
+    $(".pauseiconbig").show();
+
+    if ($('#rotate').attr('id') == 'rotate2') {
+      $('#rotate').attr('id', 'rotate');
+    }
+  });
+
+  $(".pauseiconbig").click(function () {
+    if ($('#rotate').attr('id') == 'rotate') {
+      $('#rotate').attr('id', 'rotate2');
+    }
+    $(".playiconbig").show();
+    $(".pauseiconbig").hide();
+  });
+
+
+  $(".playiconbig").click(function () {
+    $(".playiconbig").hide();
+    $(".pauseiconbig").show();
+
+    if ($('#rotate2').attr('id') == 'rotate2') {
+      $('#rotate2').attr('id', 'rotate');
+    }
+  });
+
+});
+
+// show the placeholder image on modal
+
+function displayImage(elem) {
+  var image = document.getElementById("playlistImg");
+  console.log(image.src);
+  if (elem.value == "") {
+    image.src = "images/placeholder.jpg";
+  } else {
+    image.src = elem.value;
+  }
+}
+
+  
